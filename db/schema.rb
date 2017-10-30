708a1fb5139908186e777860c5dea77df1cdcf73
sillah2010 <macktastic2014@gmail.com>
2017-10-30 15:37:14 -0400

    Added many to many for movies & users

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171030185006) do
+ActiveRecord::Schema.define(version: 20171030191128) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "favorite_movies", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "movie_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
