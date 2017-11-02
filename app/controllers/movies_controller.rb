3e59ffe972edbbe883508154e94c7a24b91bf9c9
sillah2010 <macktastic2014@gmail.com>
2017-11-01 22:42:04 -0400

    Added index and destroy route

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,23 @@
+require 'httparty'
+require 'pp'
+
 class MoviesController < ApplicationController
-
-  before_action :authenticate_user!  
-
-  def index
-  end
+    before_action :authenticate_user!  
+    def index
+        @movies = current_user.movies.map do |movie|
+          url = "http://www.omdbapi.com/?i=#{movie.omdb_id}&apikey=d31f1a94"
+          response = HTTParty.get(url)
+          response_body = JSON.parse(response.body)
+    
+          movie.title = response_body['Title']
+          movie.release_year = response_body['Year']
+          movie.rating = response_body['Rated']
+          movie
+        end
+      end
+    
+      def destroy
+        FavoriteMovie.find_by(user_id: current_user.id, movie_id: @movie.id).delete
+        redirect_to movies_url, notice: 'Movie was successfully destroyed.'
+      end
 end
